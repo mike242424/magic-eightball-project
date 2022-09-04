cbd5d865deee5befe47196afab1c6c6d537098e9
mike242424 <108441006+mike242424@users.noreply.github.com>
2022-09-04 10:54:18 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,40 @@
+const userName = "Mike";
+userName ? console.log(`Hello, ${userName}`) : console.log("Hello");
+
+let userQuestion = "will I win the lotto?";
+
+randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = "";
+
+switch (randomNumber) {
+  case 0:
+    eightBall = "It is certain";
+    break;
+  case 1:
+    eightBall = "It is decidedly so";
+    break;
+  case 2:
+    eightBall = "Reply hazy try again";
+    break;
+  case 3:
+    eightBall = "Cannot predict now";
+    break;
+  case 4:
+    eightBall = "Do not count on it";
+    break;
+  case 5:
+    eightBall = "My sources say no";
+    break;
+  case 6:
+    eightBall = "Outlook not so good";
+    break;
+  case 7:
+    eightBall = "Signs point to yes";
+    break;
+  default:
+    console.log("Not a valid question");
+}
+
+console.log(`${userName} is asking the magic eightball ${userQuestion}....`);
+console.log(`The magic eightball says: ${eightBall}`);
